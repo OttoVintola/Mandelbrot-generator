ace66109fae31931d05d5079d8c42b9099507ac5
OttoVintola <otto.e.vintola@gmail.com>
2025-12-18 19:50:46 +0200

    CUDA kernel works, segfault at x, y = 1459

--- src/generate.cu
+++ src/generate.cu
@@ -3,14 +3,30 @@
 #include <vector>
 #include <cstdio>
 #include <opencv2/opencv.hpp>
+#include <cstdlib>
+#include <cuda_runtime.h>
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
 
 struct RGB {
     uint8_t r, g, b;
 };
 
+int divup(int a, int b) {
+    return (a + b - 1)/b;
+};
+
 std::vector<float> linspace(float from, float to, int elements) {
     std::vector<float> result(elements);
-    
+
     if (elements == 0) return result;
     if (elements == 1) {
         result[0] = from;
@@ -24,22 +40,23 @@ std::vector<float> linspace(float from, float to, int elements) {
     }
 
     return result;
-
 }
 
+
 std::vector<RGB> make_palette(int max_iter) {
-    std::vector<RGB> palette;
+    std::vector<RGB> palette(max_iter);
 
     for (int i = 0; i < max_iter; ++i) {
-        RGB color;
-        color.r = i % 256;
-        color.g = (i * 5) % 256;
-        color.b = (i * 13) % 256;
-        palette.push_back(color);
+        palette[i] = {
+            uint8_t(i % 256),
+            uint8_t((i * 5) % 256),
+            uint8_t((i * 13) % 256)
+        };
     }
 
     return palette;
 }
+
 
 
 void save_image(const std::vector<RGB> &pixels, int width, int height) {
@@ -69,32 +86,35 @@ __global__ void generate(int width, int height, int max_iter,
                          const float* xSpace, const float* ySpace, 
                          const RGB* palette, RGB* result) {
 
-    
+        int i = threadIdx.x + blockIdx.x * blockDim.x;
+        int j = threadIdx.y + blockIdx.y * blockDim.y;
 
-    // for (int i = 0; i < width; ++i) {
-    //     for (int j = 0; j < height; ++j) {
+        if (i >= width || j >= height) return;
 
         float x = 0.0f;
         float y = 0.0f;
+
         int iter = 0;
+
+        float cx = xSpace[j];
+        float cy = ySpace[i];
+
+
         while ((x*x + y*y <= 4) && (iter < max_iter)) {
-            float xtemp = x*x - y*y + xSpace[j];
-            y = 2*x*y + ySpace[i];
+            float xtemp = x*x - y*y + cx;
+            y = 2*x*y + cy;
             x = xtemp;
             ++iter;
         }
 
         RGB color = palette[iter];
-        result.push_back(color);
-
-     //    }
-    // }
-
+        result[width*i + j] = color;
 
 }
 
-int main() {
-    int width, height = 0; // These control the ???
+
+int main(int argc, char* argv[]) {
+    int width, height = 0;
 
     for (int i = 0; i < argc; ++i) {
         std::string arg = argv[i];
@@ -105,29 +125,50 @@ int main() {
         }
     }
 
-    // colors 
-    std::vector<RGB> result;
-
-    // TODO: Make these into arguments
-    int xDom = 500;
-    int yDom = 500;
     int max_iter = 1000;
 
     // Scale x to be in (-2.00 to 0.47) 
-    std::vector<float> xSpace = linspace(-2.00, 0.47, width);
-    std::vector<float>* xPtr = &xSpace;
+    std::vector<float> xSpace = linspace(-2.00, 0.47, width); 
     
     // Same for y in (-1.12, 1.12)
     std::vector<float> ySpace = linspace(-1.12, 1.12, height);
-    std::vector<float>* yPtr = &ySpace;
 
     std::vector<RGB> palette = make_palette(max_iter);
 
     // Call CUDA 
     cudaStream_t stream = 0;
     size_t nSRAMBytesPerBlock = 0;
-    dim3 blockSize(16, 16); // Nor this
-    dim3 gridSize(...);
 
-    generate<<gridSize, blockSize, nSRAMBytesPerBlock, stream>>>(...); 
+    dim3 blockSize(16, 16);
+    dim3 gridSize( divup(width, blockSize.x), divup(height, blockSize.y) );
+
+
+    RGB* d_result = NULL;
+    CHECK(cudaMalloc( (void**)&d_result, width*height*sizeof(RGB)));
+
+    // Mem for xSpace and ySpace
+    float* xPtr = NULL;
+    CHECK(cudaMalloc( (void**)&xPtr, width*height*sizeof(float))); 
+    CHECK(cudaMemcpy( xPtr, xSpace.data(), width*height*sizeof(float), cudaMemcpyHostToDevice));
+
+    float* yPtr = NULL;
+    CHECK(cudaMalloc( (void**)&yPtr, width*height*sizeof(float)));
+    CHECK(cudaMemcpy( yPtr, ySpace.data(), width*height*sizeof(float), cudaMemcpyHostToDevice));
+
+    RGB* d_palette = NULL;
+    CHECK(cudaMalloc( (void**)&d_palette, max_iter*sizeof(RGB)));
+    CHECK(cudaMemcpy( d_palette, palette.data(), max_iter*sizeof(RGB), cudaMemcpyHostToDevice));
+
+    generate<<<gridSize, blockSize, nSRAMBytesPerBlock, stream>>>(width, height, max_iter, xPtr, yPtr, d_palette, d_result);
+
+    std::vector<RGB> result(width*height);
+
+    CHECK(cudaMemcpy(result.data(), d_result, width*height*sizeof(RGB), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(d_result));
+    CHECK(cudaFree(xPtr));
+    CHECK(cudaFree(yPtr));
+    CHECK(cudaFree(d_palette));
+
+    save_image(result, width, height);
+
 }
